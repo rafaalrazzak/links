a5b2e71f57509f45829f3c4838b43c52cfedd0d4
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-01-24 17:54:49 +0700

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -6,7 +6,7 @@ import twitter from "./twitter.png";
 import discord from "./discord.png";
 import facebook from "./facebook.png";
 import blog from "./blog.png";
-impirt yt from "./youtube.jpeg";
+import yt from "./youtube.jpeg";
 
 const items = [
   {
