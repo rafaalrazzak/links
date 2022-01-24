2f4c2e6782bc1daed6135b37d64fcbd64d8ca20c
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-01-24 17:52:28 +0700

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,60 +1,67 @@
-import website from "../assets/website.png";
-import github from "../assets/github.png";
-import instagram from "../assets/instagram.png";
-import secret from "../assets/secret.png";
-import twitter from "../assets/twitter.png";
-import discord from "../assets/discord.png";
-import facebook from "../assets/facebook.png";
-import blog from "../assets/blog.jpg";
+import website from "./website.png";
+import github from "./github.png";
+import instagram from "./instagram.png";
+import secret from "./secret.png";
+import twitter from "./twitter.png";
+import discord from "./discord.png";
+import facebook from "./facebook.png";
+import blog from "./blog.png";
+inport yt from "./youtube.jpeg";
 
 const items = [
   {
     title: "Website",
     subtitle: "raf-ar.web.app | Personal Web",
     image: website,
-    link: "https://raf-ar.web.app", //your personal website or portfolio  link
+    link: "https://raf-ar.web.app"
   },
   {
     title: "Blog",
-    subtitle: "Personal Blog | by vercel.pub",
+    subtitle: "Personal Blog | by Vercel",
     image: blog,
-    link: "https://s.id/b-raf", //instagram profile link
+    link: "https://s.id/b-raf"
   },
   {
     title: "GitHub",
     subtitle: "@raf-ar | Personal Github",
     image: github,
-    link: "https://github.com/raf-ar", //Github Profile link
+    link: "https://github.com/raf-ar"
   },
   {
     title: "Instagram",
     subtitle: "@rafa.ar.id | Personal Instagram",
     image: instagram,
-    link: "https://instagram.com/rafa.ar.id", //instagram profile link
+    link: "https://instagram.com/rafa.ar.id"
   },
   {
     title: "Facebook",
     subtitle: "Rafa Al Razzak | Personal Facebook",
     image: facebook,
-    link: "https://facebook.com/rafa.ar.id", //instagram profile link
+    link: "https://facebook.com/rafa.ar.id"
   },
   {
     "title": "Twitter",
     "subtitle": "@rafa_ar_id",
     "image": twitter,
-    "link": "https://twitter.com/rafa_ar_id"// twitter profile link
+    "link": "https://twitter.com/rafa_ar_id"
   },
   {
     title: "Discord",
     subtitle: "Raf#6856",
     image: discord,
-    link: "https://discordapp.com/users/431487386120093697", //instagram profile link
+    link: "https://discordapp.com/users/431487386120093697"
+  },
+  {
+    title: "Youtube",
+    subtitle: "RAF",
+    image: yt,
+    link: "https://s.id/chyt"
   },
   {
     title: "Secret Message",
     subtitle: "The Secret Messages | for someone can't talk directly",
     image: secret,
-    link: "https://s.id/s-raf", //instagram profile link
+    link: "https://s.id/s-raf"
   }
 ];
 
