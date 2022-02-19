da922ae4bdc8051a019e0a6a0b2644cca0f8f53e
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-02-19 04:12:41 +0000

    .

--- src/App.js
+++ src/App.js
@@ -8,27 +8,27 @@ import './css/skeleton.css'
 import './css/normalize.css'
 
 function App() {
-    return ( 
-        <div className = "App" >
-            <Header></Header>
-            <div className = "container row">
-                {
-                    items.map((item, i) => {
-                        return(
-                            <Card
-                            i={i}
-                            title={item.title}
-                            subtitle={item.subtitle}
-                            link={item.link}
-                            cover={item.image}>
-                            </Card>
-                        )
-                    })
-                }
-            </div>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div className="App" >
+      <Header></Header>
+      <div className="container row">
+        {
+          items.map((item, i) => {
+            return (
+              <Card
+                i={i}
+                title={item.title}
+                subtitle={item.subtitle}
+                link={item.link}
+                cover={item.image}>
+              </Card>
+            )
+          })
+        }
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
