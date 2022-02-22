b8989f933118328e2328c2c4b66ccc96ba82a48a
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-02-22 09:41:06 +0000

    .

--- src/App.js
+++ src/App.js
@@ -3,35 +3,35 @@ import Header from './components/Header'
 import Card from './components/Card'
 import Footer from './components/Footer'
 import items from './assets/items'
-import {Helmet} from ''
+import { Helmet } from 'react-helmet'
 import './css/skeleton.css'
 import './css/normalize.css'
 
 function App() {
   return (
     <div className="App" >
-      <Head
-    <Helmet >
+       <Helmet >
     <script async src="https://cdn.splitbee.io/sb.js"></script>
-    </Helmet>er />
-      <div className="container row">
-        {
-          items.map((item, i) => {
-            return (
-              <Card
-                i={i}
-                title={item.title}
-                subtitle={item.subtitle}
-                link={item.link}
-                cover={item.image}>
-              </Card>
-            )
-          })
-        }
-      </div>
-      <Footer />
+    </Helmet >
+    <Header />
+    <div className="container row">
+      {
+        items.map((item, i) => {
+          return (
+            <Card
+              i={i}
+              title={item.title}
+              subtitle={item.subtitle}
+              link={item.link}
+              cover={item.image}>
+            </Card>
+          )
+        })
+      }
     </div>
+    <Footer />
+    </div >
   );
 }
-
+ 
 export default App;
