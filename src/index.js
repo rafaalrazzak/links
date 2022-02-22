8e45b69f090d45db8d0c8468202d7e5beb868eeb
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-02-22 07:11:00 +0000

    splitbee

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import splitbee from '@splitbee/web';
+splitbee.init()
 
-ReactDOM.render( < React.StrictMode >
-    <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+ReactDOM.render(< React.StrictMode >
+  <App />
+</React.StrictMode>,
+  document.getElementById('root')
 );
