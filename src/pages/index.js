db3a9357868520ddf88fe16496c07e4376227bba
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-03-02 06:03:48 +0000

    ..

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Link | `<raf />`</title>
+				<title>Link | {`<raf />`}</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 			</Head>
 			<Layout>
