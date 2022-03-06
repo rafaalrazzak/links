98d80ab36991b9a65e5ee4b982f1d8a4520176c8
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-03-06 12:27:41 +0700

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,16 +2,14 @@ import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import Gradient from "@/components/Gradient"
 export default function Layout({ children }) {
-	return (
-		<>
-			<div>
-				<Header title="<raf />" />
-		    <Gradient />
-			<div className="flex flex-wrap">{children}
-</div>
-			<Footer />
-	   </div>
-		/div>
-		</>
-	)
+   return (
+	<>
+           <Header title="<raf />" />
+           <Gradient />
+           <div className="flex flex-wrap">
+              {children}
+           </div>
+           <Footer />
+        </>
+        )
 }
