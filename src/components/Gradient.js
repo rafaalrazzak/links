4677e44a30910fc83c699976b9e59f03602746ac
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-03-06 12:30:01 +0700

    Update Gradient.js

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -1,7 +1,7 @@
 export default function Gradient() {
    return (
        <>
-	 <div className="mx-auto md:m-0 relative w-full justify-end overflow-x-clip md:overflow-visible -z-50" >
+	 <div className="mx-auto md:m-0 relative w-full overflow-x-clip md:overflow-visible -z-50" >
             <div className="absolute top-0 -left-4 w-72 h-72 md:w-96 md:h-96 bg-amber-400 rounded-full mix-blend-multiply filter blur-2xl opacity-30 animate-blob"></div>
             <div className="absolute top-0 -right-4 w-72 h-72 md:w-96 md:h-96 bg-teal-300 rounded-full mix-blend-multiply filter blur-2xl opacity-50 animate-blob animation-delay-2000"></div>
             <div className="absolute -bottom-8 left-20 w-72 h-72 md:w-96 md:h-96 bg-pink-500 rounded-full mix-blend-multiply filter blur-2xl opacity-70 animate-blob animation-delay-4000"></div>
