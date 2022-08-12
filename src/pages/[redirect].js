61513920b04531a1632fbdd8e4d83ac1cebfb97a
rafaalrazzak <dev.rafaalrazzak@gmail.com>
2022-08-12 09:47:18 +0700

    redirect by server

--- src/pages/[redirect].js
+++ src/pages/[redirect].js
@@ -1,62 +1,28 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import client from "@/graphcms/client";
 import REDIRECT from "@/graphcms/redirect";
 
-// export async function getServerSideProps(ctx) {
-//   const { redirect } = ctx.query;
+export async function getServerSideProps(ctx) {
+  const { redirect } = ctx.query;
+  const { data } = await client.query({
+    query: REDIRECT,
+  });
+  const { redirects } = data;
+  let redirectUrl;
+  redirects.forEach(({ name, url }) => {
+    if (redirect === name) {
+      redirectUrl = url;
+    }
+  });
 
-//   let r = {
-//     name: "link",
-//     url: "/",
-//   };
+  return {
+    redirect: {
+      destination: redirectUrl ? redirectUrl : "/",
+      permanent: false,
+    },
+    props: {}, // will be passed to the page component as props
+  };
+}
 
-//   const { name, url } = r;
-
-//   if (redirect !== name) {
-//     return {
-//       redirect: {
-//         destination: "/",
-//         permanent: false,
-//       },
-//     };
-//   }
-
-//   return {
-//     redirect: {
-//       destination: url,
-//       permanent: false,
-//     },
-//     props: {}, // will be passed to the page component as props
-//   };
-// }
-
-export default function Home() {
-  const [r, setR] = useState(null);
-  const router = useRouter();
-  const clientRoute = router.query.redirect;
-
-  async function getData() {
-    await client
-      .query({
-        query: REDIRECT,
-      })
-      .then(({ data }) => setR(data.redirects));
-  }
-
-  if (r) {
-    r.forEach(({ name, url }) => {
-      if (clientRoute !== name) {
-        router.push("/");
-      } else {
-        router.push(url);
-      }
-    });
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
+export default function Reditect() {
   return <></>;
 }
