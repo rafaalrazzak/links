8e929eeff237448063a99aa8bd7f0ffa0d05401e
rafaalrazzak <dev.rafaalrazzak@gmail.com>
2023-01-15 21:09:55 +0700

    Redesign Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,50 @@
+import {useState, useEffect} from "react";
+import {MdVerified} from "react-icons/md";
 import SunIcon from "@/components/Icons/SunIcon";
 import MoonIcon from "@/components/Icons/MoonIcon";
 import Image from "next/image";
-import { useState, useEffect } from "react";
-export default function Header({ title, logo }) {
-  const [theme, setTheme] = useState();
+
+export default function Header({title, logo}){
+  const [theme, setTheme] = useState(localStorage.getItem("theme"));
 
   useEffect(() => {
-    setTheme(localStorage.getItem("theme"));
-  }, []);
-  useEffect(() => {
-    if (theme === "dark") {
+    if(theme === "dark"){
       window.document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
+    } else{
       window.document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
     }
+    localStorage.setItem("theme", theme);
   }, [theme]);
-  const isDark = theme == "dark";
-  const handleSwitchTheme = () => {
-    setTheme(isDark ? "light" : "dark");
-  };
+
+  const isDark = theme === "dark";
+  const handleSwitchTheme = () => setTheme(theme === "dark" ? "light" : "dark");
+
 
   return (
-    <div className="relative box-border flex w-full items-center justify-between p-6 font-semibold">
-      <div className="float-left box-border flex w-full items-center">
-        {logo && (
-          <Image
-            src={logo}
-            alt="logo"
-            width="40"
-            height="40"
-            className="rounded-full"
-          />
-        )}
-        <h2 className="bg-gradient-to-r from-teal-600 to-teal-400 bg-clip-text px-3 text-xl font-bold text-transparent">
-          {title}
-        </h2>
-      </div>
+      <div className="sticky z-50 top-0 box-border flex w-full items-center justify-between p-6 font-semibold ">
+        <div
+            className="bg-primary-100/40 backdrop-blur-xl flex justify-between flex-1 p-3 rounded-full items-center dark:bg-primary-800/40">
 
-      <button
-        className="flex h-8 w-16 cursor-pointer select-none items-center justify-center rounded-full bg-gradient-to-r from-teal-600 to-teal-400 text-center text-base text-white no-underline drop-shadow-xl"
-        onClick={handleSwitchTheme}
-      >
-        {isDark ? <SunIcon /> : <MoonIcon />}
-      </button>
-    </div>
-  );
+          {logo && (
+              <Image
+                  src={logo}
+                  alt="logo"
+                  width="40"
+                  height="40"
+                  className="rounded-full"
+              />)}
+
+          <h2 className="bg-gradient-to-r from-teal-600 gap-1 flex items-center to-teal-400 bg-clip-text px-3  font-bold text-transparent">
+            {title}
+            <MdVerified className="inline-block text-blue-500 text-xs"/>
+          </h2>
+
+          <button
+              className="flex h-10 w-12 cursor-pointer select-none items-center justify-center rounded-full bg-gradient-to-r from-teal-600 to-teal-400 text-center text-base text-white no-underline "
+              onClick={handleSwitchTheme}
+          >
+            {isDark ? <SunIcon/> : <MoonIcon/>}
+          </button>
+        </div>
+      </div>);
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,11 +7,9 @@ import QUERY from "@/graphcms/query";
 
 export async function getServerSideProps(ctx) {
   const clientUrl = ctx.query.r;
-  const { data } = await client.query({
+  const {data: {urls}} = await client.query({
     query: QUERY,
   });
-
-  const { urls } = data;
 
   if (clientUrl) {
     return {
@@ -34,22 +32,23 @@ export default function Home({ urls }) {
         title="Links | raf"
         description="a simple website for any social media link"
       />
-
-      <Layout headTitle="< raf />" headLogo={raf}>
-        {urls?.map((item, i) => {
-          return (
-            <Card
-              key={item.id}
-              i={i}
-              title={item.title}
-              subtitle={item.description}
-              link={encodeURIComponent(item.url)}
-              cover={item.thumbnail.url}
-              cW={item.thumbnail.width}
-              cH={item.thumbnail.height}
-            ></Card>
-          );
-        })}
+      <Layout headTitle="rafaalrazzak" headLogo={raf}>
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3">
+          {urls?.map((item, i) => {
+            return (
+                <Card
+                    key={item.id}
+                    i={i}
+                    title={item.title}
+                    subtitle={item.description}
+                    link={encodeURIComponent(item.url)}
+                    cover={item.thumbnail.url}
+                    cW={item.thumbnail.width}
+                    cH={item.thumbnail.height}
+                />
+            );
+          })}
+        </div>
       </Layout>
     </div>
   );
