1395ec51276a81b569c770f635c8594509135371
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2023-05-03 09:46:58 +0700

    env

---
+++ src/libs/utils/generateRandomString.js
@@ -0,0 +1,14 @@
+function generateRandomString() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const length = Math.floor(Math.random() * 10) + 5;
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result;
+}
+
+export default generateRandomString;
