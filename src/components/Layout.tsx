17b5cfac2fbf26de570e1f45ee507fc8615368bc
Rafa Al Razzak <94893946+rafaalrazzak@users.noreply.github.com>
2023-10-31 12:00:34 +0000

    remove unused

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,13 @@
 import { Toaster } from "react-hot-toast";
 import { Header } from "@/components/Header";
 import Footer from "@/components/Footer";
-import Bg from "@/components/Bg";
 import { ReactNode } from "react";
 
 interface LayoutProps {
   children: ReactNode;
-  headTitle?: string;
-  headLogo?: string;
 }
 
-export function Layout({ children, headTitle, headLogo }: LayoutProps) {
+export function Layout({ children }: LayoutProps) {
   return (
     <div className="mx-auto flex h-screen max-w-xl flex-col items-center  justify-around px-4 gap-4">
       <Header />
